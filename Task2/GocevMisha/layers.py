ef794b9edfe4b39fff9081c26f17bfc41448e27e
GocevMisha <migocev@gmail.com>
2020-03-12 00:23:10 +0300

    Add files via upload

--- Task2/GocevMisha/layers.py
+++ Task2/GocevMisha/layers.py
@@ -4,39 +4,84 @@ import numpy as np
 def l2_regularization(W, reg_strength):
     """
     Computes L2 regularization loss on weights and its gradient
-
     Arguments:
       W, np array - weights
       reg_strength - float value
-
     Returns:
       loss, single value - l2 regularization loss
       gradient, np.array same shape as W - gradient of weight by l2 loss
     """
-    # TODO: Copy from the previous assignment
-    raise Exception("Not implemented!")
+    
+    loss = reg_strength*(W**2).sum()
+    grad = 2 * reg_strength * W
+
     return loss, grad
+
+
+
+def softmax(predictions):
+    '''
+    Computes probabilities from scores
+
+    Arguments:
+      predictions, np array, shape is either (N) or (batch_size, N) -
+        classifier output
+
+    Returns:
+      probs, np array of the same shape as predictions - 
+        probability for every class, 0..1
+    '''
+    # TODO implement softmax
+    # Your final implementation shouldn't have any loops
+    probs = predictions.copy()
+    if probs.ndim == 1:
+        probs -= np.max(predictions)
+        probs = np.exp(probs) / np.sum(np.exp(probs))
+    else:
+        probs -= np.max(predictions, axis=1).reshape(-1, 1)
+        probs = np.exp(probs) / np.sum(np.exp(probs), axis=1).reshape(-1, 1)
+
+    return probs
+
+
+def cross_entropy_loss(probs, target_index):
+    '''
+    Computes cross-entropy loss
+    Arguments:
+      probs, np array, shape is either (N) or (batch_size, N) -
+        probabilities for every class
+      target_index: np array of int, shape is (1) or (batch_size) -
+        index of the true class for given sample(s)
+    Returns:
+      loss: single value
+    '''
+    if type(target_index) == int: return -np.log(probs[target_index])
+    return - np.mean(np.log(probs[range(target_index.shape[0]), target_index]))
 
 
 def softmax_with_cross_entropy(preds, target_index):
     """
     Computes softmax and cross-entropy loss for model predictions,
     including the gradient
-
     Arguments:
       predictions, np array, shape is either (N) or (batch_size, N) -
         classifier output
       target_index: np array of int, shape is (1) or (batch_size) -
         index of the true class for given sample(s)
-
     Returns:
       loss, single value - cross-entropy loss
       dprediction, np array same shape as predictions - gradient of predictions by loss value
     """
-    # TODO: Copy from the previous assignment
-    raise Exception("Not implemented!")
+    softmax_res = softmax(preds)    
+    loss = cross_entropy_loss(softmax_res, target_index)
+    dprediction = softmax_res
+    if type(target_index) == int: 
+        dprediction[target_index] -= 1
+        return loss, dprediction
+    dprediction[range(target_index.shape[0]), target_index] -= 1
 
-    return loss, d_preds
+    return loss, dprediction/target_index.shape[0]
+    
 
 
 class Param:
@@ -55,26 +100,25 @@ class ReLULayer:
         pass
 
     def forward(self, X):
-        # TODO: Implement forward pass
         # Hint: you'll need to save some information about X
         # to use it later in the backward pass
-        raise Exception("Not implemented!")
+        self.X = X
+        return np.maximum(0,X)       
 
     def backward(self, d_out):
         """
         Backward pass
-
         Arguments:
         d_out, np array (batch_size, num_features) - gradient
            of loss function with respect to output
-
         Returns:
         d_result: np array (batch_size, num_features) - gradient
           with respect to input
         """
-        # TODO: Implement backward pass
         # Your final implementation shouldn't have any loops
-        raise Exception("Not implemented!")
+        # raise Exception("Not implemented!")
+        d_result = d_out * (self.X > 0)
+        
         return d_result
 
     def params(self):
@@ -89,20 +133,18 @@ class FullyConnectedLayer:
         self.X = None
 
     def forward(self, X):
-        # TODO: Implement forward pass
         # Your final implementation shouldn't have any loops
-        raise Exception("Not implemented!")
+        self.X = X
+        return np.dot(X, self.W.value) + self.B.value
 
     def backward(self, d_out):
         """
         Backward pass
         Computes gradient with respect to input and
         accumulates gradients within self.W and self.B
-
         Arguments:
         d_out, np array (batch_size, n_output) - gradient
            of loss function with respect to output
-
         Returns:
         d_result: np array (batch_size, n_input) - gradient
           with respect to input
@@ -115,7 +157,9 @@ class FullyConnectedLayer:
         # It should be pretty similar to linear classifier from
         # the previous assignment
 
-        raise Exception("Not implemented!")
+        self.W.grad += self.X.T @ d_out
+        self.B.grad += np.sum(d_out, axis=0)[:, np.newaxis].T
+        d_input = d_out @ self.W.value.T
 
         return d_input
 
--- Task2/GocevMisha/model.py
+++ Task2/GocevMisha/model.py
@@ -9,7 +9,6 @@ class TwoLayerNet:
     def __init__(self, n_input, n_output, hidden_layer_size, reg):
         """
         Initializes the neural network
-
         Arguments:
         n_input, int - dimension of the model input
         n_output, int - number of classes to predict
@@ -17,56 +16,69 @@ class TwoLayerNet:
         reg, float - L2 regularization strength
         """
         self.reg = reg
-        # TODO Create necessary layers
-        raise Exception("Not implemented!")
+        self.dense1 = FullyConnectedLayer(n_input, n_output)
+        self.act1 = ReLULayer()
+        self.dense2 = FullyConnectedLayer(n_output, n_output)
 
     def compute_loss_and_gradients(self, X, y):
         """
         Computes total loss and updates parameter gradients
         on a batch of training examples
-
         Arguments:
         X, np array (batch_size, input_features) - input data
         y, np array of int (batch_size) - classes
         """
         # Before running forward and backward pass through the model,
         # clear parameter gradients aggregated from the previous pass
-        # TODO Set parameter gradient to zeros
+        # Set parameter gradient to zeros
         # Hint: using self.params() might be useful!
-        raise Exception("Not implemented!")
+        params = self.params()
+        for key in params.keys():
+            params[key].grad = np.zeros_like(params[key].value)
         
-        # TODO Compute loss and fill param gradients
+      
+        # Compute loss and fill param gradients
         # by running forward and backward passes through the model
-        
+        out = self.dense2.forward(self.act1.forward(self.dense1.forward(X)))
+
+        loss, grad = softmax_with_cross_entropy(out, y)
+
+        self.dense1.backward(self.act1.backward(self.dense2.backward(grad)))
+
         # After that, implement l2 regularization on all params
         # Hint: self.params() is useful again!
-        raise Exception("Not implemented!")
+
+        for key in params.keys():
+            l2_loss, l2_grad = l2_regularization(params[key].value, self.reg)
+            loss += l2_loss
+            params[key].grad += l2_grad
 
         return loss
 
     def predict(self, X):
         """
         Produces classifier predictions on the set
-
         Arguments:
           X, np array (test_samples, num_features)
-
         Returns:
           y_pred, np.array of int (test_samples)
         """
-        # TODO: Implement predict
+        # Implement predict
         # Hint: some of the code of the compute_loss_and_gradients
         # can be reused
-        pred = np.zeros(X.shape[0], np.int)
+ 
+        out = self.dense2.forward(self.act1.forward(self.dense1.forward(X)))
 
-        raise Exception("Not implemented!")
-        return pred
+        return out.argmax(axis=1)
 
     def params(self):
         result = {}
 
-        # TODO Implement aggregating all of the params
+        result['W1'] = self.dense1.W
+        result['W2'] = self.dense2.W
 
-        raise Exception("Not implemented!")
+        result['B1'] = self.dense1.B
+        result['B2'] = self.dense2.B
+
 
         return result
